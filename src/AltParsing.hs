c49301dbd1a2f17dd40985946c47840747304b04
kawaharashotaro <kkawaharasshoutarou@gmail.com>
2019-01-26 15:58:41 +0900

    AltParsing

--- src/AltParsing.hs
+++ src/AltParsing.hs
@@ -1,2 +1,48 @@
+{-# LANGUAGE QuasiQuotes #-}
 module AltParsing where
 
+import Control.Applicative
+import Text.Trifecta
+import Text.RawString.QQ
+
+
+type NumberOrString = Either Integer String
+newtype MyName = MyName String deriving Show
+
+a = "blah"
+b = "123"
+c = "123blah789"
+
+parseNos :: Parser NumberOrString
+parseNos =
+  skipMany (oneOf "\n") *>
+  (Left <$> integer) <|>
+  (Right <$> some letter) <*
+  skipMany (oneOf "\n")
+  
+
+{-
+main = do
+  let p f = parseString f mempty
+  print $ p (some letter) a
+  print $ p integer b
+  print $ p parseNos a
+  print $ p parseNos b
+  print $ p (many parseNos) c
+  print $ p (some parseNos) c
+-}
+
+main = do
+  let p f = parseString f mempty
+  print $ p parseNos eitherOr
+
+eitherOr :: String
+eitherOr =[r|
+123
+abc
+456
+def
+|]
+
+--r :: QuasiQuoter
+--r = undefined
--- src/Data/Ini.hs
+++ src/Data/Ini.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Data.Ini where
+
+import           Control.Applicative
+import           Data.ByteString.Char8 (ByteString)
+import           Data.Char
+import           Data.Map
+import qualified Data.Map as M
+import           Data.Text (Text)
+import           Data.Text.IO as TIO
+import           Text.RawString.QQ
+import           Text.Trifecta
+import           Test.Hspec
+
+
+headerEx :: ByteString
+headerEx = "[blah]"
+
+newtype Header = Header String deriving (Eq, Ord, Show)
+
 
